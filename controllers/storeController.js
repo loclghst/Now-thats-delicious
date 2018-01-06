da9c3261dc1c473abc78eada4a2b1fdb781bd9aa
Manish Kumar Sharma <loclghst@gmail.com>
2018-01-06 23:30:24 +0530

    Multiple Query Promises with async await

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -85,9 +85,11 @@ exports.getStoreBySlug = async (req, res, next) => {
 }
 
 exports.getStoreByTag = async (req, res) => {
-  const tags = await Store.getTagsList()
-  // hilight tags
   const tag = req.params.tag
-  res.render('tag', { tags, title: 'Tags', tag })
+  const tagQuery = tag || { $exists: true }
+  const tagsPromise = Store.getTagsList()
+  const storesPromise = Store.find({ tags: tagQuery })
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise])
+  res.render('tag', { tags, title: 'Tags', tag, stores })
 }
 
