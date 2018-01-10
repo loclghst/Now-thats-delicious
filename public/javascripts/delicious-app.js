c18ba8c03c0bb02fad90c29ed68992dc2bc2e848
Manish Kumar Sharma <loclghst@gmail.com>
2018-01-10 13:49:07 +0530

    no dist files

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,18 +1,3 @@
-import '../sass/style.scss'
+import '../sass/style.scss';
 
-import { $, $$ } from './modules/bling'
-import autocomplete from './modules/autocomplete'
-import typeAhead from './modules/typeAhead'
-import makeMap from './modules/map'
-import ajaxHeart from './modules/heart';
-
-// so the '$' is shorthand here for document.querySelector
-// using blingjs $$$$$!!!
-autocomplete( $('#address'), $('#lat'), $('#lng'))
-
-typeAhead( $('.search') )
-
-makeMap( $('#map') )
-
-const heartForms = $$('form.heart')
-heartForms.on('submit', ajaxHeart)
+import { $, $$ } from './modules/bling';
